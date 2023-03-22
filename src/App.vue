861d611cceac9f41bc1fd5a07bf3d1682c2b712f
yee.zhang <yee.zhang@yunlsp.com>
2023-03-22 16:47:13 +0800

    feat: 添加多格式保存逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { computed, nextTick, ref, shallowRef } from 'vue'
+import { computed, ref, shallowRef } from 'vue'
 import numbro from 'numbro'
 import { saveAs } from 'file-saver'
-import zhCN from '@/assets/zh-CN.json'
+import all from '@/assets/all.json'
 
 type LocalesObj = {
   [key: string]: string | LocalesObj
@@ -15,9 +15,13 @@ type LocalesKey = {
   locales: string | object
 }
 
-const dataRes = ref<LocalesObj>(zhCN)
+type AllFormatObj = {
+  [key: string]: Record<string, string>
+}
 
-const keyList = computed(() => {
+const dataRes = ref<LocalesObj | AllFormatObj>(all)
+
+const getSingleKeyObjList = () => {
   const keyObjList: LocalesKey[] = []
   /**
    * 铺平
@@ -40,11 +44,25 @@ const keyList = computed(() => {
   }
   flatObject(dataRes.value)
   return keyObjList
+}
+
+const getMultipleKeyObjList = () => {
+  const keyObjList: LocalesKey[] = []
+  const keys = Object.keys(dataRes.value)
+  keys.forEach(key => {
+    keyObjList.push({ key, locales: dataRes.value[key]['zh-CN'] })
+  })
+  return keyObjList
+}
+
+const keyList = computed(() => {
+  return isMultipleFormat.value ? getMultipleKeyObjList() : getSingleKeyObjList()
 })
 
 const translateDelay = ref<number>(1500)
 const saveFileName = ref<string>('')
 const isTranslating = ref<boolean>(false)
+const isMultipleFormat = ref<boolean>(true)
 const translatePercent = ref<number>(0)
 const showPercentText = computed<string>(() =>
   numbro(translatePercent.value).format({ output: 'percent', mantissa: 1 })
@@ -56,7 +74,11 @@ const previewFile = () => {
   inputRef.value?.click()
 }
 
-const saveDataToFile = () => {
+/**
+ * 单语言格式
+ */
+const saveDataToFile = async () => {
+  await getTranslateData()
   const keyMapElementList = document.querySelectorAll('.key-map')
   const blobData: any = {}
   const setDeepObject = (keyNested: string, value: string) => {
@@ -79,39 +101,81 @@ const saveDataToFile = () => {
     const keyNested = element.getElementsByTagName('pre')[0].dataset
       .key as string
     // 放置value
-    const objValue = element.getElementsByClassName('value')[0].innerHTML
+    const objValue = element.getElementsByClassName('value')[0].innerText
     setDeepObject(keyNested, objValue)
   })
   const blob = new Blob([JSON.stringify(blobData)], {
     type: 'text/plain;charset=utf-8',
   })
-  saveAs(blob, `${saveFileName.value ? saveFileName.value : 'translate'}.json`)
+  saveAs(blob, `${saveFileName.value || 'zh-CN'}.json`)
   setTimeout(() => {
     const body = document.documentElement
     body.scrollTop = 0
   }, translateDelay.value)
 }
 
-const getTranslateData = () => {
-  if (isTranslating.value) return
-  isTranslating.value = true
-  translatePercent.value = 0
-
-  const body = document.documentElement
-  body.scrollTop = 0
-
-  const timer = setInterval(() => {
-    const pageHeight = body.clientHeight
-    const endScrollTop = body.offsetHeight - pageHeight
-    body.scrollTop = body.scrollTop + pageHeight
-    translatePercent.value = body.scrollTop / endScrollTop
-    if (body.scrollTop >= endScrollTop) {
-      clearInterval(timer)
-      isTranslating.value = false
-      translatePercent.value = 1
-      setTimeout(saveDataToFile, translateDelay.value)
+/**
+ * 多语言格式
+ */
+const saveDataMultipleToFile = async () => {
+  await getTranslateData()
+  const keyMapElementList = document.querySelectorAll('.key-map')
+  const blobData: any = all
+  const locale = saveFileName.value || 'zh-CN'
+  keyMapElementList.forEach(element => {
+    const key =  element.getElementsByTagName('pre')[0].dataset
+      .key as string
+    const value = element.getElementsByClassName('value')[0].innerText
+    if (blobData[key] === undefined) {
+      blobData[key] = {}
     }
+    blobData[key][locale] = value
+  })
+  const blob = new Blob([JSON.stringify(blobData)], {
+    type: 'text/plain;charset=utf-8',
+  })
+  saveAs(blob, `all.json`)
+  setTimeout(() => {
+    const body = document.documentElement
+    body.scrollTop = 0
   }, translateDelay.value)
+}
+
+const handleSaveFile = () => {
+  if (isMultipleFormat.value) {
+    saveDataMultipleToFile()
+  } else {
+    saveDataToFile()
+  }
+}
+
+/**
+ * 滚动翻译
+ */
+const getTranslateData = () => {
+  if (isTranslating.value) return Promise.reject('正在翻译')
+  return new Promise(resolve => {
+    isTranslating.value = true
+    translatePercent.value = 0
+
+    const body = document.documentElement
+    body.scrollTop = 0
+
+    const timer = setInterval(() => {
+      const pageHeight = body.clientHeight
+      const endScrollTop = body.offsetHeight - pageHeight
+      body.scrollTop = body.scrollTop + pageHeight
+      translatePercent.value = body.scrollTop / endScrollTop
+      if (body.scrollTop >= endScrollTop) {
+        clearInterval(timer)
+        isTranslating.value = false
+        translatePercent.value = 1
+        setTimeout(() => {
+          resolve(true)
+        }, translateDelay.value)
+      }
+    }, translateDelay.value)
+  })
 }
 
 const handleSelectFile = (e: any) => {
@@ -131,7 +195,7 @@ const handleSelectFile = (e: any) => {
       <a href="https://vitejs.dev" target="_blank">
         <img src="/vite.svg" class="logo" alt="Vite logo" />
       </a>
-      <span>+</span>
+      <span>&emsp;+&emsp;</span>
       <a href="https://vuejs.org/" target="_blank">
         <img src="/vue.svg" class="logo vue" alt="Vue logo" />
       </a>
@@ -139,7 +203,9 @@ const handleSelectFile = (e: any) => {
         <img src="/translate-tip.gif" alt="" />
       </div>
     </div>
-    <div class="h1">翻译</div>
+    <div class="h1">
+      <code>翻译</code>
+    </div>
     <button style="margin-left: 10px" @click="previewFile">
       <code>预览文件</code>
       <input
@@ -149,9 +215,15 @@ const handleSelectFile = (e: any) => {
         @change="handleSelectFile"
       />
     </button>
-    <button style="margin-left: 10px" @click="getTranslateData">
+    <button style="margin-left: 10px" @click="handleSaveFile">
       <code>保存文件</code>
     </button>
+    <span>
+      <input id="format" type="checkbox" v-model="isMultipleFormat">
+      <label for="format">
+        <code>多语言格式</code>
+      </label>
+    </span>
     <select style="margin-left: 10px" v-model="translateDelay">
       <option :value="1000">
         <code>1s</code>
@@ -166,7 +238,7 @@ const handleSelectFile = (e: any) => {
     <input
       style="margin-left: 10px"
       v-model="saveFileName"
-      placeholder="保存文件名"
+      placeholder="语言国际码(zh-CN)"
     />
     <div style="flex: 1"></div>
     <div class="percent">{{ showPercentText }}</div>
@@ -191,17 +263,19 @@ header {
   background: white;
   display: flex;
   align-items: center;
+  box-shadow: 0 2px 5px #e5e5e5;
+
+  &:hover {
+    .logo-tip .tip-hover {
+      display: block;
+    }
+  }
 
   .logo-tip {
+    padding: 0 2em;
     position: relative;
     display: flex;
     align-items: center;
-
-    &:hover {
-      .tip-hover {
-        display: block;
-      }
-    }
 
     .tip-hover {
       display: none;
@@ -218,7 +292,7 @@ header {
 
   .logo {
     height: 2em;
-    padding: 1.5em;
+    //padding: 1.5em;
     will-change: filter;
   }
 
